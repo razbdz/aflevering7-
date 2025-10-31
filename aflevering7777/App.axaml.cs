f4f0fa78dbce9d9d8cbea8c19353e09dbdfa1e8d
Razije <s236154@dtu.dk>
2025-10-31 22:18:55 +0100

    ny ændring

--- aflevering7777/App.axaml.cs
+++ aflevering7777/App.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace aflevering7777
+namespace aflevering7777   // ← lille l, præcis sådan
 {
     public partial class App : Application
     {
@@ -15,7 +15,7 @@ namespace aflevering7777
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow();
+ 
             }
 
             base.OnFrameworkInitializationCompleted();
